98f7cae00c87d8db5d6c7276864f39f923522dc2
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-05-29 23:49:58 -0300

    fix:  responsive grid

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader, CardTitle } from "@/app/_components/_shadcn/ui/card";
+import { Card, CardTitle } from "@/app/_components/_shadcn/ui/card";
 
 import Image from "next/image";
 
@@ -24,29 +24,27 @@ const products = [
 export default function ProductsPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <div className="text-center mb-6">
-        <h1 className="font-bold text-2xl mb-2">Productos</h1>
-        <h2 className="text-lg">Encontrá aquello que buscás!</h2>
-      </div>
-
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-2 gap-y-[8px]">
+      <h1 className="text-2xl font-bold">Productos</h1>
+      <h2 className="text-lg mt-3 mb-6">Encontrá aquello que buscás!</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[15px]">
         {products.map((product) => (
           <Card
             key={product}
             className="w-[226px] h-[81px] flex items-center justify-center mx-auto"
           >
-            <CardHeader className="p-2 flex flex-row items-center justify-center space-x-2 w-full">
-              <Image
-                src=""
-                alt={product}
-                width={40}
-                height={40}
-                className="object-contain rounded"
-              />
-              <CardTitle className="text-sm font-medium truncate w-full overflow-hidden text-ellipsis text-left">
+            <div className="flex flex-row items-center justify-center w-full h-full">
+              {/* TODO: Corroborar como alinear el icono y el texto de otra forma */}
+              <div className="w-12 h-12 flex items-center justify-center">
+                <Image
+                  src=""
+                  alt="icon"
+                  className="object-contain rounded w-full h-full"
+                />
+              </div>
+              <CardTitle className="ml-4 text-center w-full">
                 {product}
               </CardTitle>
-            </CardHeader>
+            </div>
           </Card>
         ))}
       </div>

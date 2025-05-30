dc4e454c01f3584488a745b8ee6353f0216cfa5a
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-05-30 10:12:16 -0300

    style: body

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardTitle } from "@/app/_components/_shadcn/ui/card";
+import { Card, CardHeader, CardTitle } from "@/app/_components/_shadcn/ui/card";
 
 import Image from "next/image";
 
@@ -23,25 +23,23 @@ const products = [
 
 export default function ProductsPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <h1 className="text-2xl font-bold">Productos</h1>
+    <div className="  min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-2xl font-extrabold">Productos</h1>
       <h2 className="text-lg mt-3 mb-6">Encontrá aquello que buscás!</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[15px]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3.5">
         {products.map((product) => (
-          <Card
-            key={product}
-            className="w-[226px] h-[81px] flex items-center justify-center mx-auto"
-          >
-            <div className="flex flex-row items-center justify-center w-full h-full">
-              {/* TODO: Corroborar como alinear el icono y el texto de otra forma */}
-              <div className="w-12 h-12 flex items-center justify-center">
+          <Card key={product} className="h-[81px] w-full p-3">
+            <div className="w-full h-full flex items-center gap-2 ">
+              <div className="size-12 flex items-center justify-center">
                 <Image
-                  src=""
+                  src="https://placehold.co/64x64"
                   alt="icon"
+                  width="64"
+                  height="64"
                   className="object-contain rounded w-full h-full"
                 />
               </div>
-              <CardTitle className="ml-4 text-center w-full">
+              <CardTitle className="w-full text-center overflow-hidden text-ellipsis font-bold text-2xl">
                 {product}
               </CardTitle>
             </div>

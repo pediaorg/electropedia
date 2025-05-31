694c6319a5037c6d24a668f1087dbf4a54cc1c6f
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-05-31 16:00:35 -0300

    feat: Brands-page

---
+++ src/app/brands/page.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import { Button } from "@/app/_components/_shadcn/ui/button";
+import { Card, CardContent } from "@/app/_components/_shadcn/ui/card";
+const brands = [
+  "Samsung",
+  "LG",
+  "Whirlpool",
+  "Philco",
+  "Mabe",
+  "Frigidaire",
+  "Columbia",
+  "GE Appliances",
+  "Gafa",
+  "Panasonic",
+  "Electrolux",
+];
+
+export default function Brands() {
+  return (
+    <div className="DEBUG p-6 min-h-screen">
+      <div className="flex items-center gap-4 mb-4">
+        <Image
+          src=""
+          alt="Heladeras"
+          width="64"
+          height="64"
+          className="size-12 border rounded-full bg-blue-50"
+        />
+        <div className="overflow-hidden text-ellipsis">
+          <h1 className="text-4xl font-extrabold">Heladeras</h1>
+          <p className="text-xl text-muted-foreground">Guias de reparacion</p>
+          <p className="text-sm text-muted-foreground">
+            {brands.length} marcas
+          </p>
+        </div>
+      </div>
+      <div className="hidden sm:flex items-center justify-between mb-4">
+        <h2 className="font-normal text-3xl">Marcas</h2>
+        <Button variant="outline">Filtrar ▾</Button>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5">
+        {brands.map((brand) => (
+          <Card key={brand} className="bg-amber-100 cursor-pointer">
+            <CardContent className="flex justify-center items-center h-full">
+              <Image src="" alt="" width="64" height="64" className="h-full" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}

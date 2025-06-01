a8ec104614de3d1d2cbb9064d55b58013e0d2dc5
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-05-31 22:03:03 -0300

    fix: body

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,11 @@
-"use client";
-
 import { Card, CardTitle } from "@/app/_components/_shadcn/ui/card";
 import Image from "next/image";
 import { ProductSelector } from "@/app/_components/dialogs";
-import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from "@/app/_components/_shadcn/ui/dialog";
 
 const products = [
   "Heladeras",
@@ -25,8 +27,6 @@ const products = [
 ];
 
 export default function ProductsPage() {
-  const [selectedProduct, setSelectedProduct] = useState<string | null>(null);
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
       <h1 className="text-4xl font-extrabold">Productos</h1>
@@ -34,35 +34,31 @@ export default function ProductsPage() {
       <hr className="w-3xl border-t-2 border-[#cfc7bb] mb-6" />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3.5">
         {products.map((product) => (
-          <Card
-            key={product}
-            className="w-full p-3"
-            onClick={() => setSelectedProduct(product)}
-          >
-            <div className="w-full h-full flex items-center gap-2 ">
-              <div className="size-12 flex items-center justify-center">
-                <Image
-                  src="https://placehold.co/64x64"
-                  alt="icon"
-                  width="64"
-                  height="64"
-                  className="object-contain rounded w-full h-full"
-                />
-              </div>
-              <CardTitle className="w-full text-center overflow-hidden text-ellipsis font-medium text-2xl">
-                {product}
-              </CardTitle>
-            </div>
-          </Card>
+          <Dialog key={product}>
+            <DialogTrigger asChild>
+              <Card className="w-full p-3">
+                <div className="w-full h-full flex items-center gap-2 ">
+                  <div className="size-12 flex items-center justify-center">
+                    <Image
+                      src=""
+                      alt="icon"
+                      width="64"
+                      height="64"
+                      className="object-contain rounded w-full h-full"
+                    />
+                  </div>
+                  <CardTitle className="w-full text-center overflow-hidden text-ellipsis font-medium text-2xl">
+                    {product}
+                  </CardTitle>
+                </div>
+              </Card>
+            </DialogTrigger>
+            <DialogContent>
+              <ProductSelector />
+            </DialogContent>
+          </Dialog>
         ))}
       </div>
-      {selectedProduct && (
-        <ProductSelector
-          product={selectedProduct}
-          open={!!selectedProduct}
-          onClose={() => setSelectedProduct(null)}
-        />
-      )}
     </div>
   );
 }

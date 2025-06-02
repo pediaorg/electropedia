97cffa58a2784b80b63cea99438299a54b5b809b
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-06-02 20:49:04 -0300

    fix: improve body page

--- src/app/discussion/page.tsx
+++ src/app/discussion/page.tsx
@@ -15,24 +15,26 @@ import {
     AvatarImage
 } from "@/app/_components/_shadcn/ui/avatar"
 import { Button } from "@/app/_components/_shadcn/ui/button"
+import { ThumbsUp } from 'lucide-react';
+import { ThumbsDown } from 'lucide-react';
 
 export default function OneDiscussion() {
-    return <div className="DEBUG grid gap-10 mx-auto py-10 container">
-        <div className="flex justify-between items-center self-center gap-20">
+    return <div className="grid gap-10 mx-auto py-10 container w-full">
+        <div className="flex justify-between items-center self-center gap-20 w-full">
             <div className="flex gap-10">
 
                 {/* Foto y nombre de usuario */}
-                <div className="flex flex-col mx-5 items-center">
+                <div className="flex flex-col mx-5 items-center text-center">
                     <div>
-                        <Avatar className="size-36 border border-sidebar-foreground m-5">
-                            <AvatarImage src="https://placehold.co/600x400" className="mx-auto max-w-screen" />
+                        <Avatar className="size-36 border border-sidebar-foreground mb-5">
+                            <AvatarImage src="https://placehold.co/144x144" className="mx-auto max-w-screen" />
                             <AvatarFallback>JC</AvatarFallback>
                         </Avatar>
                     </div>
-                    <h1 className="font-bold text-2xl">
+                    <h1 className="font-bold text-2xl mb-1">
                         Juan I. Casareski
                     </h1>
-                    <p className="font-normal">
+                    <p className="font-normal mb-2">
                         @JuanICasareski
                     </p>
                 </div>
@@ -41,14 +43,14 @@ export default function OneDiscussion() {
                 <div className="flex flex-col justify-around">
                     <div>
                         <p className="text-xs text-foreground font-extralight">
-                            [Fecha de publicación]
+                            11/08/2024
                         </p>
                         <h1 className="text-4xl font-bold">
-                            [Pregunta]
+                            Alguien sabe cómo puedo arreglar un auto-mate que se le rompió la bombilla?
                         </h1>
                     </div>
                     <p className="text-2xl text-foreground">
-                        [Descripción]
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et fuga quis amet minima laborum molestias tempore sunt aperiam ad! Exercitationem vitae est quam, animi quia non atque architecto sequi sit?
                     </p>
 
                     {/* Botones */}
@@ -67,18 +69,18 @@ export default function OneDiscussion() {
             {/* Producto */}
             <div className="grid gap-1 bg-primary-foreground rounded-sm border-border border shadow-xl p-1">
                 <div>
-                    <Image src="https://placehold.co/600x400" alt="Imagen de un producto" width="222" height="148" className="mx-auto max-w-screen" />
+                    <Image src="https://placehold.co/222x148" alt="Imagen de auto-mate" width="222" height="148" className="mx-auto max-w-screen" />
                 </div>
                 <p className="font-semibold text-center text-foreground">
-                    [Nombre del producto]
+                    Auto-mate Stanley
                 </p>
             </div>
         </div>
 
-        <div className="grid gap-10">
+        <div className="grid gap-10 w-full">
 
             {/* Respuestas */}
-            <div className="flex">
+            <div className="flex justify-between">
                 <h1 className="text-5xl font-bold">
                     Respuestas
                 </h1>
@@ -90,31 +92,31 @@ export default function OneDiscussion() {
             {/* Respuesta 1 */}
             <Card className="bg-primary-foreground border-primary border-2 p-0">
                 <div className="flex gap-x-10">
-                    {/* Foto y nombre de usuario */}    
-                    <div className="flex flex-col mx-5 items-center">
+                    {/* Foto y nombre de usuario */}
+                    <div className="flex flex-col mx-5 items-center place-content-center">
                         <div>
-                            <Avatar className="size-36 border border-sidebar-foreground m-5">
-                                <AvatarImage src="https://placehold.co/600x400" className="mx-auto max-w-screen"/>
+                            <Avatar className="size-36 border border-sidebar-foreground mb-5">
+                                <AvatarImage src="https://placehold.co/144x144" className="mx-auto max-w-screen"/>
                                 <AvatarFallback>JC</AvatarFallback>
                             </Avatar>
                         </div>
-                        <h1 className="text-center font-bold text-2xl">
-                            Juan I. Casareski
+                        <h1 className="text-center font-bold text-2xl mb-1">
+                            Manuel Núñez
                         </h1>
-                        <p className="text-center font-normal">
-                            @JuanICasareski
+                        <p className="text-center font-normal mb-2">
+                            @ManuelNuñez
                         </p>
                     </div>
 
                     {/* Respuesta */}
-                    <div className="flex flex-col gap-2 justify-around w-full">
-                        <CardHeader className="flex flex-row justify-between">
-                            <div>
+                    <div className="flex flex-col pr-1 pt-1 justify-between w-full">
+                        <CardHeader className="flex flex-row justify-between p-0">
+                            <div className="py-11">
                                 <CardDescription className="text-xs text-foreground font-extralight">
-                                    [Fecha de publicación]
+                                    11/08/2024
                                 </CardDescription>
                                 <CardTitle className="text-2xl text-foreground">
-                                    [Respuestas]
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi harum pariatur nobis aliquam, nemo quisquam ipsam necessitatibus voluptas officia consequuntur cumque dolorum mollitia, voluptate reprehenderit velit voluptatem molestias doloribus praesentium.
                                 </CardTitle>
                             </div>
                             <Badge className="text-base">
@@ -122,12 +124,14 @@ export default function OneDiscussion() {
                             </Badge>
                         </CardHeader>
 
-                        <CardContent className="flex gap-5">
-                            <p className="text-foreground font-extralight">
-                                Me gusta
+                        <CardContent className="flex gap-2 py-15 px-0">
+                            <ThumbsUp/>
+                            <p className="text-foreground font-medium mr-3">
+                                10
                             </p>
-                            <p className="text-foreground font-extralight">
-                                No me gusta
+                            <ThumbsDown/>
+                            <p className="text-foreground font-medium">
+                                1
                             </p>
                         </CardContent>
                     </div>
@@ -135,43 +139,47 @@ export default function OneDiscussion() {
             </Card>
 
             {/* Respuesta 2 */}
-            <Card className="bg-primary-foreground border-border border-2">
+            <Card className="bg-primary-foreground border-border border-2 p-0">
                 <div className="flex gap-x-10">
                     {/* Foto y nombre de usuario */}    
-                    <div className="flex flex-col mx-5 items-center">
+                    <div className="flex flex-col mx-5 items-center place-content-center">
                         <div>
                             <Avatar className="size-36 border border-sidebar-foreground m-5">
-                                <AvatarImage src="https://placehold.co/600x400" className="mx-auto max-w-screen" />
+                                <AvatarImage src="https://placehold.co/144x144" className="mx-auto max-w-screen"/>
                                 <AvatarFallback>JC</AvatarFallback>
                             </Avatar>
                         </div>
-                        <h1 className="text-center font-bold text-2xl">
-                            Juan I. Casareski
+                        <h1 className="text-center font-bold text-2xl mb-1">
+                            Santiago Ribecca
                         </h1>
-                        <p className="text-center font-normal">
-                            @JuanICasareski
+                        <p className="text-center font-normal mb-2">
+                            @SantiagoRibecca
                         </p>
                     </div>
 
                     {/* Respuesta */}
-                    <div className="flex flex-col gap-2 justify-around">
-                        <div className="flex flex-col">
-                            <p className="text-xs text-foreground font-extralight">
-                                [Fecha de publicación]
-                            </p>
-                            <h2 className="text-2xl text-foreground">
-                                [Respuestas]
-                            </h2>
-                        </div>
+                    <div className="flex flex-col pr-1 pt-1 justify-between w-full">
+                        <CardHeader className="flex flex-row justify-between p-0">
+                            <div className="py-11">
+                                <CardDescription className="text-xs text-foreground font-extralight">
+                                    11/10/2024
+                                </CardDescription>
+                                <CardTitle className="text-2xl text-foreground pr-30">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores perferendis rem quibusdam laudantium accusamus, autem, eius, vel quis aut blanditiis ipsa? Adipisci odio ullam suscipit explicabo cumque sunt aut deleniti.
+                                </CardTitle>
+                            </div>
+                        </CardHeader>
 
-                        <div className="flex gap-5">
-                            <p className="text-foreground font-extralight">
-                                Me gusta
+                        <CardContent className="flex gap-2 py-15 px-0">
+                            <ThumbsUp/>
+                            <p className="text-foreground font-medium mr-3">
+                                2
                             </p>
-                            <p className="text-foreground font-extralight">
-                                No me gusta
+                            <ThumbsDown/>
+                            <p className="text-foreground font-medium">
+                                0
                             </p>
-                        </div>
+                        </CardContent>
                     </div>
                 </div>
             </Card>

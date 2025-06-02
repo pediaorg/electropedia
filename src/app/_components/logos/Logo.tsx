56fd6c414d39fb9a006c7d3609ece470933a7b8c
JuanICasareski <juani.casareski@gmail.com>
2025-06-02 17:42:05 -0300

    feat: logos components

---
+++ src/app/_components/logos/Logo.tsx
@@ -0,0 +1,6 @@
+import { Hammer } from "lucide-react";
+
+export default function Logo() {
+  // TODO: Reemplazar logo por el martillo correcto
+  return <Hammer className="fill-secondary stroke-secondary" size="35" />;
+}

2553c9fa5316590617db0d3e22e649775a8bdc64
JuanICasareski <juani.casareski@gmail.com>
2025-06-02 17:44:44 -0300

    fix: set logo size to 9

--- src/app/_components/logos/Logo.tsx
+++ src/app/_components/logos/Logo.tsx
@@ -2,5 +2,5 @@ import { Hammer } from "lucide-react";
 
 export default function Logo() {
   // TODO: Reemplazar logo por el martillo correcto
-  return <Hammer className="fill-secondary stroke-secondary" size="35" />;
+  return <Hammer className="fill-secondary stroke-secondary size-9" />;
 }

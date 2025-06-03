cc82d52c5188ac666349c6202f26ad21790b279d
ManuelNuniez <manuelnunununiez@gmail.com>
2025-06-03 17:38:23 -0300

    feat: Corrección de colores y formato

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -21,18 +21,18 @@ export default async function SubscriptionPage(){
 
 
     return(
-        <div className="flex flex-col items-center mx-auto container py-10 gap-12 DEBUG">
+        <div className="flex flex-col items-center mx-auto container py-10 gap-12">
             <h1 className="font-bold text-6xl">Electropedia Plus</h1>
             <p className="font-medium text-2xl">Hazte usuario premium y aprovecha todas las funcionalidades disponibles</p>
-            <div>
-                <Tabs defaultValue="account" className="items-center">
-                    <TabsList className="gap-3 ">
-                        <TabsTrigger value="account">Mensual</TabsTrigger>
-                        <TabsTrigger value="password">Anual</TabsTrigger>
+            <div className="w-full">
+                <Tabs defaultValue="monthly" className="items-center">
+                    <TabsList className="gap-3">
+                        <TabsTrigger value="monthly">Mensual</TabsTrigger>
+                        <TabsTrigger value="yearly">Anual</TabsTrigger>
                     </TabsList>
-                    <TabsContent value="account">
-                        <div className="grid grid-cols-3 gap-15"> 
-                            <Card className="w-64 text-center border rounded-xl bg-primary-foreground">
+                    <TabsContent value="monthly" className="w-full">
+                        <div className="grid grid-cols-3 gap-10 max-w-215 mx-auto DEBUG"> 
+                            <Card className="w-full text-center border border-black rounded-xl bg-primary-foreground">
                                 <CardHeader>
                                 <CardTitle>Title</CardTitle>
                                 </CardHeader>
@@ -46,7 +46,7 @@ export default async function SubscriptionPage(){
                                         <li key={i}>{item}</li>
                                         ))}
                                     </ul>
-                                    <Button className="w-full">Button</Button>
+                                    <Button variant="outline" className="w-full bg-black text-white hover:bg-white hover:text-black border-black">Button</Button>
                                 </CardContent>
                             </Card>
 
@@ -65,7 +65,7 @@ export default async function SubscriptionPage(){
                                     <li key={i}>{item}</li>
                                     ))}
                                 </ul>
-                                <Button variant="outline" className="w-full border-white text-white hover:text-black hover:bg-white">
+                                <Button variant="outline" className="w-full border-white text-black hover:text-white hover:bg-black">
                                     Button
                                 </Button>
                                 </CardContent>
@@ -86,28 +86,28 @@ export default async function SubscriptionPage(){
                                     <li key={i}>{item}</li>
                                     ))}
                                 </ul>
-                                <Button className="w-full">Button</Button>
+                                <Button variant="outline" className="w-full bg-black text-white hover:bg-white hover:text-black border-black">Button</Button>
                                 </CardContent>
                             </Card>
                         </div>
                     </TabsContent>
-                    <TabsContent value="password">
-                        <div className="grid grid-cols-3 gap-15"> 
-                            <Card className="w-64 text-center border rounded-xl bg-primary-foreground">
+                    <TabsContent value="yearly" className="w-full">
+                        <div className="grid grid-cols-3 gap-10 max-w-215 mx-auto"> 
+                            <Card className="w-full text-center border border-black rounded-xl bg-primary-foreground">
                                 <CardHeader>
                                 <CardTitle>Title</CardTitle>
                                 </CardHeader>
                                 <CardContent>
                                     <div className="text-3xl font-bold mb-2">
                                         <span className="text-lg align-super">$</span>500
-                                        <span className="text-sm font-normal text-foreground">/mo</span>
+                                        <span className="text-sm font-normal text-foreground">/year</span>
                                     </div>
                                     <ul className="text-muted-foreground space-y-1 text-left list-disc list-inside mb-4">
                                         {Array(5).fill("List item").map((item, i) => (
                                         <li key={i}>{item}</li>
                                         ))}
                                     </ul>
-                                    <Button className="w-full">Button</Button>
+                                    <Button variant="outline" className="w-full bg-black text-white hover:bg-white hover:text-black border-black">Button</Button>
                                 </CardContent>
                             </Card>
 
@@ -119,14 +119,14 @@ export default async function SubscriptionPage(){
                                 <CardContent>
                                 <div className="text-3xl font-bold mb-2">
                                     <span className="text-lg align-super">$</span>500
-                                    <span className="text-sm font-normal text-white/70">/mo</span>
+                                    <span className="text-sm font-normal text-foreground">/year</span>
                                 </div>
                                 <ul className="space-y-1 text-left list-disc list-inside mb-4">
                                     {Array(5).fill("List item").map((item, i) => (
                                     <li key={i}>{item}</li>
                                     ))}
                                 </ul>
-                                <Button variant="outline" className="w-full border-white text-white hover:text-black hover:bg-white">
+                                <Button variant="outline" className="w-full border-white text-black hover:text-white hover:bg-black">
                                     Button
                                 </Button>
                                 </CardContent>
@@ -140,14 +140,14 @@ export default async function SubscriptionPage(){
                                 <CardContent>
                                 <div className="text-3xl font-bold mb-2">
                                     <span className="text-lg align-super">$</span>500
-                                    <span className="text-sm font-normal text-muted-foreground">/mo</span>
+                                    <span className="text-sm font-normal text-foreground">/year</span>
                                 </div>
                                 <ul className="text-muted-foreground space-y-1 text-left list-disc list-inside mb-4">
                                     {Array(5).fill("List item").map((item, i) => (
                                     <li key={i}>{item}</li>
                                     ))}
                                 </ul>
-                                <Button className="w-full">Button</Button>
+                                <Button variant="outline" className="w-full bg-black text-white hover:bg-white hover:text-black border-black">Button</Button>
                                 </CardContent>
                             </Card>
                         </div>

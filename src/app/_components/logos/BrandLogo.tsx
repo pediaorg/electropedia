9856dd25d460c70469c2fe915dc8bcc98e44a8d8
Juan I. Casareski <juani.casareski@gmail.com>
2025-06-03 23:16:20 -0300

    feat: eletropedia logo

--- src/app/_components/logos/BrandLogo.tsx
+++ src/app/_components/logos/BrandLogo.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 export default function BrandLogo() {
   return (
-    <div className="flex gap-3 items-center">
+    <div className="flex gap-1.5 items-center">
       <Logo />
       <span className="text-2xl font-bold">Electropedia</span>
     </div>
--- src/app/_components/logos/Logo.tsx
+++ src/app/_components/logos/Logo.tsx
@@ -1,6 +1,17 @@
-import { Hammer } from "lucide-react";
-
 export default function Logo() {
-  // TODO: Reemplazar logo por el martillo correcto
-  return <Hammer className="fill-secondary stroke-secondary size-9" />;
+  // return <Hammer className="fill-secondary stroke-secondary size-9" />;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      className="fill-secondary size-9"
+    >
+      <path
+        fill="currentColor"
+        d="M17 8V2h3a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1zm-2 14a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1V8H2.5V6.074a1 1 0 0 1 .496-.863L8.5 2H15z"
+      />
+    </svg>
+  );
 }

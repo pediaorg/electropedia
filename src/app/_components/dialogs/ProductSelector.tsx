22581eb82e2c5d7c56c578f27a81e0ae2d5edf13
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-06-11 15:31:03 -0300

    fix: buttons display and colours

--- src/app/_components/dialogs/ProductSelector.tsx
+++ src/app/_components/dialogs/ProductSelector.tsx
@@ -1,16 +1,18 @@
+import { Button } from "../_shadcn/ui/button";
+
 export default function ProductSelector() {
   return (
-    <div>
+    <div className="DEBUG">
       <h1 className="text-3xl font-extrabold text-center">Productos</h1>
-      <p className="text-center text-gray-500 mt-1 mb-1">¿A dónde vamos?</p>
+      <p className="text-center text-foreground mt-1 mb-1">¿A dónde vamos?</p>
       <hr className="my-2" />
-      <div className="flex gap-4 justify-center mt-2">
-        <button className="bg-[#cfc7bb] text-2xl font-semibold px-8 py-2 rounded w-40 focus:outline-none">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-4 justify-center mt-2">
+        <Button className="bg-input text-2xl font-bold text-secundary px-6 py-2 rounded flex-1">
           Guías
-        </button>
-        <button className="bg-[#cfc7bb] text-2xl font-semibold px-8 py-2 rounded w-40 focus:outline-none">
+        </Button>
+        <Button className="bg-input text-2xl font-bold text-secundary px-6 py-2 rounded flex-1">
           Repuestos
-        </button>
+        </Button>
       </div>
     </div>
   );

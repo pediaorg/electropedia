45dfeb31a338cbec69f58d201055ffcccf8b3c4e
Santiago Ribecca <ribeccasantiago@gmail.com>
2025-06-11 15:57:16 -0300

    wip

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,6 +4,7 @@ import { ProductSelector } from "@/app/_components/dialogs";
 import {
   Dialog,
   DialogContent,
+  DialogTitle,
   DialogTrigger,
 } from "@/app/_components/_shadcn/ui/dialog";
 
@@ -28,10 +29,10 @@ const products = [
 
 export default function ProductsPage() {
   return (
-    <div className="flex flex-col items-center justify-center py-10 px-8 container mx-auto">
+    <div className="flex flex-col items-center justify-center py-10 px-8">
       <h1 className="text-4xl font-extrabold">Productos</h1>
       <h2 className="text-lg mt-3 mb-6">Encontrá aquello que buscás!</h2>
-      <hr className="w-3xl border-t-2 border-[#cfc7bb] mb-6" />
+      <hr className="w-full border-t-2 border-border mb-6" />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3.5">
         {products.map((product) => (
           <Dialog key={product}>
@@ -40,20 +41,21 @@ export default function ProductsPage() {
                 <div className="w-full h-full flex items-center gap-2 ">
                   <div className="size-12 flex items-center justify-center">
                     <Image
-                      src=""
+                      src="https://placehold.co/64x64"
                       alt="icon"
                       width="64"
                       height="64"
                       className="object-contain rounded w-full h-full"
                     />
                   </div>
-                  <CardTitle className="w-full text-center overflow-hidden text-ellipsis font-medium text-2xl">
+                  <CardTitle className="w-full text-center overflow-hidden text-ellipsis font-medium text-base sm:text-lg md:text-xl lg:text-2xl">
                     {product}
                   </CardTitle>
                 </div>
               </Card>
             </DialogTrigger>
             <DialogContent>
+              <DialogTitle />
               <ProductSelector />
             </DialogContent>
           </Dialog>

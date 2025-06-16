cd4ff912d9c62d8db7217f01b20867eac2f8be1d
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-06-16 12:05:38 -0300

    fix: vertical responsive view in respuestas and card color changing

--- src/app/discussion/page.tsx
+++ src/app/discussion/page.tsx
@@ -89,66 +89,65 @@ export default function OneDiscussion() {
         </div>
       </div>
 
-        <div className="grid gap-10 w-full">
+      <div className="grid gap-10 w-full">
+        {/* Respuestas */}
+        <div className="flex justify-between">
+          <h1 className="text-5xl font-bold">Respuestas</h1>
 
-            {/* Respuestas */}
-            <div className="flex justify-between">
-                <h1 className="text-5xl font-bold">
-                    Respuestas
-                </h1>
-                
-                {/* Filtros */}
+          {/* Filtros */}
+        </div>
 
+        {/* Respuesta 1 */}
+        <Card className="bg-input border-primary border-2 p-0">
+          <div className="flex flex-col lg:flex-row gap-10">
+            {/* Foto y nombre de usuario */}
+            <div className="flex flex-col mx-5 items-center place-content-center">
+              <div className="pt-8">
+                <Avatar className="size-52 border border-sidebar-foreground mb-5 sm:size-80 lg:size-36">
+                  <AvatarImage
+                    src="https://placehold.co/144x144"
+                    className="mx-auto max-w-screen"
+                  />
+                  <AvatarFallback>JC</AvatarFallback>
+                </Avatar>
+              </div>
+              <h1 className="text-center font-bold text-3xl sm:text-4xl lg:text-2xl mb-1">
+                Manuel Núñez
+              </h1>
+              <p className="text-center font-normal mb-2 sm:text-lg lg:text-base">
+                @ManuelNuñez
+              </p>
             </div>
 
-            {/* Respuesta 1 */}
-            <Card className="bg-primary-foreground border-primary border-2 p-0">
-                <div className="flex gap-x-10">
-                    {/* Foto y nombre de usuario */}
-                    <div className="flex flex-col mx-5 items-center place-content-center">
-                        <div>
-                            <Avatar className="size-36 border border-sidebar-foreground mb-5">
-                                <AvatarImage src="https://placehold.co/144x144" className="mx-auto max-w-screen"/>
-                                <AvatarFallback>JC</AvatarFallback>
-                            </Avatar>
-                        </div>
-                        <h1 className="text-center font-bold text-2xl mb-1">
-                            Manuel Núñez
-                        </h1>
-                        <p className="text-center font-normal mb-2">
-                            @ManuelNuñez
-                        </p>
-                    </div>
-
-                    {/* Respuesta */}
-                    <div className="flex flex-col pr-1 pt-1 justify-between w-full">
-                        <CardHeader className="flex flex-row justify-between p-0">
-                            <div className="py-11">
-                                <CardDescription className="text-xs text-foreground font-extralight">
-                                    11/08/2024
-                                </CardDescription>
-                                <CardTitle className="text-2xl text-foreground">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi harum pariatur nobis aliquam, nemo quisquam ipsam necessitatibus voluptas officia consequuntur cumque dolorum mollitia, voluptate reprehenderit velit voluptatem molestias doloribus praesentium.
-                                </CardTitle>
-                            </div>
-                            <Badge className="text-base">
-                                Más votada
-                            </Badge>
-                        </CardHeader>
-
-                        <CardContent className="flex gap-2 py-15 px-0">
-                            <ThumbsUp/>
-                            <p className="text-foreground font-medium mr-3">
-                                10
-                            </p>
-                            <ThumbsDown/>
-                            <p className="text-foreground font-medium">
-                                1
-                            </p>
-                        </CardContent>
-                    </div>
+            {/* Respuesta */}
+            <div className="flex flex-col pl-5 pr-1 pt-1 w-full lg:pl-0">
+              <CardHeader className="flex flex-col-reverse p-0">
+                <div className="pt-5 pr-4">
+                  <CardDescription className="text-xs text-foreground font-extralight">
+                    11/08/2024
+                  </CardDescription>
+                  <CardTitle className="text-2xl text-foreground">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Commodi harum pariatur nobis aliquam, nemo quisquam ipsam
+                    necessitatibus voluptas officia consequuntur cumque dolorum
+                    mollitia, voluptate reprehenderit velit voluptatem molestias
+                    doloribus praesentium.
+                  </CardTitle>
                 </div>
-            </Card>
+                <div className="place-self-end">
+                  <Badge className="text-base">Más votada</Badge>
+                </div>
+              </CardHeader>
+
+              <CardContent className="flex gap-2 py-10 px-0 lg:py-15">
+                <ThumbsUp />
+                <p className="text-foreground font-medium mr-3">10</p>
+                <ThumbsDown />
+                <p className="text-foreground font-medium">1</p>
+              </CardContent>
+            </div>
+          </div>
+        </Card>
 
             {/* Respuesta 2 */}
             <Card className="bg-primary-foreground border-border border-2 p-0">

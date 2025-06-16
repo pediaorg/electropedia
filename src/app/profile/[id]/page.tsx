6ba008fe54aefed25a7c3b938af7caf57a75e040
ManuelNuniez <manuelnunununiez@gmail.com>
2025-06-16 18:34:55 -0300

    Cambios estéticos

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -15,7 +15,10 @@ import {
 } from "@/app/_components/_shadcn/ui/avatar";
 import { Button } from "@/app/_components/_shadcn/ui/button";
 import { Separator } from "@/app/_components/_shadcn/ui/separator";
-import { ScrollArea } from "@/app/_components/_shadcn/ui/scroll-area";
+import {
+  ScrollArea,
+  ScrollBar,
+} from "@/app/_components/_shadcn/ui/scroll-area";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -23,6 +26,7 @@ import {
   DropdownMenuTrigger,
 } from "@/app/_components/_shadcn/ui/dropdown-menu";
 import React from "react";
+import { Scrollbar } from "@radix-ui/react-scroll-area";
 
 type PageProps = { params: Promise<{ id: string }> };
 export default async function ProfilePage(props: PageProps) {
@@ -35,28 +39,30 @@ export default async function ProfilePage(props: PageProps) {
   );
   return (
     <div className="flex flex-col mx-auto container py-10 gap-15 px-8 ">
-      <div className="w-full flex flex-col md:flex-row justify-between DEBUG">
+      <div className="w-full flex flex-col md:my-9 md:flex-row justify-between ">
         <div className="flex w-full md:w-fit">
           <div className="flex flex-col items-center">
-            <Avatar className="size-16 md:size-40 lg:size-64">
+            <Avatar className="size-16 md:size-40 lg:size-64 border border-black">
               <AvatarImage src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             {/*boton visible solo en mobile*/}
-            <Button variant={"secondary"} className="md:hidden w-fit">
+            <Button variant={"secondary"} className="md:hidden w-fit mt-2">
               Editar
             </Button>
           </div>
 
-          <div className="flex flex-col px-2 flex-1">
+          <div className="flex flex-col md:mt-4 ml-10 xl:ml-14 flex-1">
             <div className="flex flex-col md:flex-row text-xl md:text-2xl xl:text-3xl">
               <p className="font-bold">Usuario</p>
-              <p className="font-normal overflow-hidden text-ellipsis">@{id}</p>
+              <p className="font-normal overflow-hidden md:ml-2 text-ellipsis">
+                @{id}
+              </p>
             </div>
             <p className="font-extralight text-sm md:text-xl italic">
               Miembro desde: 31/05/2025
             </p>
-            <div className="font-normal text-lg md:text-2xl md:space-y-3">
+            <div className="font-normal text-lg md:text-2xl md:space-y-3 md:my-5 xl:space-y-5">
               <p>15 soluciones</p>
               <p>24 comentarios</p>
               <p>3 guias</p>
@@ -67,53 +73,24 @@ export default async function ProfilePage(props: PageProps) {
           <Button variant={"secondary"}>Editar perfil</Button>
         </div>
       </div>
-      {/* <div>
-        <Card className="w-full flex flex-row p-3">
-          <Avatar className="size-16 md:size-40 lg:size-16">
-            <AvatarImage src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <div className="w-full flex flex-col justify-center">
-            <CardHeader className="p-0">
-              <div className="flex flex-row space-x-2 truncate">
-                <CardTitle className="font-bold text-3xl">Usuario</CardTitle>
-                <CardTitle className="font-normal text-3xl overflow-hidden text-ellipsis">
-                  @{id}
-                </CardTitle>
-              </div>
-              <CardDescription className="font-extralight text-xl italic">
-                Miembro desde: 31/05/2025
-              </CardDescription>
-              <CardAction className="m-3">
-                <Button variant={"secondary"}>Editar perfil</Button>
-              </CardAction>
-            </CardHeader>
-            <CardContent className="font-normal text-2xl space-y-3 p-0">
-              <p>15 soluciones</p>
-              <p>24 comentarios</p>
-              <p>3 guias</p>
-            </CardContent>
-          </div>
-        </Card>
-      </div> */}
 
       <div className="w-full flex flex-col">
         {/* manuales */}
-        <Card className="flex flex-col space-x-4 p-4">
-          <CardHeader>
+        <Card className="flex flex-col space-x-4 p-4 border-none shadow-none">
+          <CardHeader className="pl-0 ml-0">
             <CardTitle className="text-4xl font-medium">Manuales</CardTitle>
           </CardHeader>
 
           <Separator />
 
-          <ScrollArea className="h-40">
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
+          <ScrollArea className="h-40 whitespace-nowrap">
+            <div className="flex gap-2">
               {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
                 <div
                   key={item}
-                  className="p-2 flex flex-row bg-primary-foreground items-center h-18 border border-border rounded-sm"
+                  className="p-2 flex flex-row bg-input items-center h-20 w-70 md:h-28 md:w-96 border border-border rounded-sm"
                 >
-                  <Avatar className="size-16 border border-border">
+                  <Avatar className="size-16 md:size-24 border border-border">
                     <AvatarImage
                       src={
                         "https://samsungar.vtexassets.com/arquivos/ids/192242-1200-auto?width=1200&height=auto&aspect=true"
@@ -121,22 +98,25 @@ export default async function ProfilePage(props: PageProps) {
                     />
                   </Avatar>
                   <CardContent className="p-0 truncate">
-                    <p className="ml-2 text-xl font-semibold text-blue-600 truncate">
+                    <p className="ml-2 text-lg md:text-xl font-bold text-blue-600 truncate">
                       Heladera Samsung M345 texto extra para probar
                     </p>
-                    <p className="ml-2 text-medium">16/4/2025</p>
+                    <p className="ml-2 text-sm md:text-lg text-medium">
+                      16/4/2025
+                    </p>
                   </CardContent>
                 </div>
               ))}
             </div>
+            <ScrollBar orientation="horizontal" />
           </ScrollArea>
         </Card>
       </div>
 
       <div className="w-full flex flex-col">
         {/* actividad */}
-        <Card>
-          <CardHeader>
+        <Card className="border-none shadow-none">
+          <CardHeader className="pl-0 ml-0">
             <CardTitle className="text-4xl font-medium">Actividad</CardTitle>
           </CardHeader>
           <Separator />
@@ -159,32 +139,31 @@ export default async function ProfilePage(props: PageProps) {
           </DropdownMenu>
 
           <div>
-            <ScrollArea className="h-48 p-2">
+            <ScrollArea className="h-40 md:h-72 p-2">
               <div className="space-y-3">
                 {[1, 2, 3, 4].map((item) => (
                   <Card
                     key={item}
-                    className="p-3 flex border borde-border bg-primary-foreground"
+                    className="p-3 md:h-32 flex border borde-border bg-input"
                   >
                     <div className="flex justify-between items-center">
-                      <div className="flex gap-2">
-                        <div className="flex">
-                          <Avatar className="size-14 border border-border">
+                      <div className="flex  gap-2 DEBUG">
+                        <div className="flex items-center">
+                          <Avatar className="size-16 md:size-24 border border-border">
                             <AvatarImage
                               src={
                                 "https://samsungar.vtexassets.com/arquivos/ids/192242-1200-auto?width=1200&height=auto&aspect=true"
                               }
                             />
                           </Avatar>
-                          <CardContent className="p-0 ">
-                            <p className="ml-2 text-xl font-semibold text-blue-600 truncate">
+                          <CardContent className="p-0">
+                            <p className="ml-2 text-lg md:text-2xl font-bold text-blue-600 truncate">
                               Foro Heladera
                             </p>
-                            <p className="ml-2 text-medium">16/4/2025</p>
                           </CardContent>
                         </div>
                       </div>
-                      <span className="text-xs text-muted-foreground">
+                      <span className="text-sm md:text-lg text-muted-foreground">
                         16/4/2025
                       </span>
                     </div>

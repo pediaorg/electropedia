5a19d64b231136ac25ab06ba54288912bdd50824
Santiago Ferreyra <129809907+santiagoferreyraa@users.noreply.github.com>
2025-06-22 17:06:36 -0300

    feat: discussion page (#13)
    * feat(alpha): discussion-page
    
    * fix: improve body page
    
    * style: container positioning and min padding
    
    * fix: vertical responsive view & bigger avatar
    
    * fix: new padding in pregunta
    
    * fix: change color background prdoucto
    
    * fix: vertical responsive view in respuestas and card color changing
    
    * fix: buttons improve
    
    * fix: respuesta 2
    
    * fix: discussion buttons wrap
    
    * fix: thumbs rating component
    
    * feat: user profile header component
    
    * feat: answers component
    
    * refactor: make discussion answers function
    
    * refactor: UserAvatar Props changed from interface to type
    
    * feat: creation of RespondDiscussion component
    
    * refactor: name of UserAvatar function changed
    
    * refactor: ThumbsRating changed to component
    
    * chore: discussion page types and fns names
    
    * chore: rename new answer dialog
    
    ---------
    
    Co-authored-by: Juan I. Casareski <juani.casareski@gmail.com>

---
+++ src/app/_components/dialogs/NewAnswer.tsx
@@ -0,0 +1,35 @@
+import { Input } from "@/app/_components/_shadcn/ui/input";
+import { Card } from "@/app/_components/_shadcn/ui/card";
+import { Bold, Italic, Link2, Image, Plus, Send } from "lucide-react";
+
+export default function NewAnswer() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="border-b border-secondary pb-5">
+        <h1 className="text-5xl font-bold text-center w-full">Respuesta</h1>
+      </div>
+      <div className="flex flex-col gap-3">
+        <Card className="flex flex-row items-center bg-input p-2 w-full justify-between">
+          <div className="flex gap-2">
+            <Bold className="size-5" />
+            <Italic className="size-5" />
+            <Link2 className="size-5" />
+            <Image className="size-5" />
+            <div className="border-l-2 border-secondary px-1">
+              <Plus className="size-5" />
+            </div>
+          </div>
+          <div className="place-items-center place-content-center rounded-full bg-background size-8">
+            <Send className="size-5" />
+          </div>
+        </Card>
+        <Input
+          type="answer"
+          id="answer"
+          placeholder="Escriba su respuesta..."
+          className="bg-input border-border"
+        />
+      </div>
+    </div>
+  );
+}

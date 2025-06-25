02182dd60d3ed3ad8f3ae732ea2438367dee98b2
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-06-25 00:14:32 -0300

    chore: link and new images in discussions

--- src/app/_components/discussions/Discussion.tsx
+++ src/app/_components/discussions/Discussion.tsx
@@ -11,23 +11,23 @@ type Props = {
 
 export default function Discussion({ name, img, date, answers }: Props) {
   return (
-    <Link href="discussion/">
-      <Card key={name} className="p-3 flex w-full">
-        <div className="flex justify-between items-center gap-2">
-          <div className="flex gap-2 justify-center items-center">
-            <Avatar className="size-14 border bg-white">
-              <AvatarImage src={img} alt="Heladera" />
-            </Avatar>
+    <Card key={name} className="p-3 flex w-full">
+      <div className="flex justify-between items-center gap-2">
+        <div className="flex gap-2 justify-center items-center">
+          <Avatar className="size-14 border bg-white items-center justify-center">
+            <AvatarImage src={img} alt="Heladera" className="size-10" />
+          </Avatar>
+          <Link href="discussion/" className="">
             <span className="text-sm font-medium text-blue-800 underline">
               {name}
             </span>
-          </div>
-          <div className="flex flex-col-reverse items-center lg:flex-row gap-2 text-xs text-foreground">
-            <p className="truncate">{answers} respuestas</p>
-            <p>{date}</p>
-          </div>
+          </Link>
         </div>
-      </Card>
-    </Link>
+        <div className="flex flex-col-reverse items-center lg:flex-row gap-2 text-xs text-foreground">
+          <p className="truncate">{answers} respuestas</p>
+          <p>{date}</p>
+        </div>
+      </div>
+    </Card>
   );
 }

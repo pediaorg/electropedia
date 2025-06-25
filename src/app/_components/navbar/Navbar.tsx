67dfd1a7e1e1ce746a78ef5e0ffb37e3911e506f
Juan I Casareski <66040481+JuanICasareski@users.noreply.github.com>
2025-06-25 12:16:58 -0300

    Feat/navbar logged in state (#25)
    * feat: landing page mockup login

--- src/app/_components/navbar/Navbar.tsx
+++ src/app/_components/navbar/Navbar.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import { BrandLogo } from "@/app/_components/logos";
 import { Button } from "@/app/_components/_shadcn/ui/button";
-import { Menu } from "lucide-react";
 import { Input } from "@/app/_components/_shadcn/ui/input";
+import { Menu, Bell, User } from "lucide-react";
+import Link from "next/link";
+
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const path = usePathname();
+
+  const isLogged = path === "/";
+
   return (
     <div className="p-5 border-b-2 flex justify-between lg:gap-13 md:gap-8 gap-2">
       <div className="flex gap-4 lg:gap-6 item-center justify-between w-full md:w-fit ">
@@ -25,16 +34,45 @@ export default function Navbar() {
       </div>
 
       <div className="sm:flex hidden gap-y-1 gap-x-5 items-center">
-        <Button
-          variant="secondary"
-          size="sm"
-          className="bg-input text-secondary px-4 hover:bg-input/80"
-        >
-          Ingresar
-        </Button>
-        <Button variant="secondary" size="sm" className="px-4">
-          Registrarse
-        </Button>
+        {isLogged ? (
+          <>
+            <Link href="/products">
+              <Button
+                variant="secondary"
+                size="sm"
+                className="bg-input text-secondary px-4 hover:bg-input/80 cursor-pointer"
+              >
+                Ingresar
+              </Button>
+            </Link>
+            <Link href="/products">
+              <Button
+                variant="secondary"
+                size="sm"
+                className="px-4 cursor-pointer"
+              >
+                Registrarse
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Button
+              variant="secondary"
+              size="icon"
+              className="bg-input text-secondary px-4 hover:bg-input/80 rounded-full cursor-pointer"
+            >
+              <Bell className="size-5 fill-white stroke-white" />
+            </Button>
+            <Button
+              variant="secondary"
+              size="icon"
+              className="bg-input text-secondary px-4 hover:bg-input/80 rounded-full cursor-pointer"
+            >
+              <User className="size-5 fill-white stroke-white" />
+            </Button>
+          </>
+        )}
       </div>
     </div>
   );

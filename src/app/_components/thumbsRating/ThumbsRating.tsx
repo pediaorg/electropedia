3cf44090820b0eff5bdf48af830c3bc6ea3d01d8
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-06-24 23:48:33 -0300

    refactor: link in avatar & product

--- src/app/_components/thumbsRating/ThumbsRating.tsx
+++ src/app/_components/thumbsRating/ThumbsRating.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ThumbsRating(props: Props) {
   return (
-    <div className="flex gap-2 py-10 lg:py-15">
+    <div className="flex gap-2 py-10 lg:py-8">
       <ThumbsUp />
       <p className="text-foreground font-medium mr-3">{props.likes}</p>
       <ThumbsDown />

886f95d5cfbbad1b1413914a7b14e51a61933d0a
Juan I Casareski <66040481+JuanICasareski@users.noreply.github.com>
2025-07-09 17:40:15 -0300

    chore: revert db examples

--- src/app/test/db/page.tsx
+++ src/app/test/db/page.tsx
@@ -1,7 +1,7 @@
 import { api } from "@/trpc/server";
 
 export default async function DbTestPage() {
-  const response = await api.products.get({ id: "6865b6f33e299ff65f4ab505" });
+  const response = await api.test.db();
 
   console.log(response);
 

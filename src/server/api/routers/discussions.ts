a2a15c6ce7ac1c9d193a9c795a8cc8bd10119c88
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-07-11 16:05:31 -0300

    fix: changes in discussions querys

--- src/server/api/routers/discussions.ts
+++ src/server/api/routers/discussions.ts
@@ -1,22 +1,19 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { z } from "zod";
 import { Discussion } from "@/server/db/models";
 import { TRPCError } from "@trpc/server";
 
 export const discussionsRouter = createTRPCRouter({
-  get: protectedProcedure
-    .input(z.object({ id: z.string() })) // => type { name: string }
-    .query(async ({ input }) => {
-      const response = await Discussion.findById(input.id);
+  getAll: protectedProcedure.query(async () => {
+    const response = await Discussion.find();
 
-      if (!response) {
-        throw new TRPCError({
-          code: "NOT_FOUND",
-        });
-      }
+    if (!response) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+      });
+    }
 
-      return response;
-    }),
+    return response;
+  }),
   //   db: protectedProcedure.query(async () => {
   //     const user = await User.create({
   //       name: "Juan I. Casareski",

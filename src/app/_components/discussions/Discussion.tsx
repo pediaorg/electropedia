53012bbfd79dc7f35c58b3bbb59bbef16d600300
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-07-11 16:02:28 -0300

    refactor: change discussion link & type of date

--- src/app/_components/discussions/Discussion.tsx
+++ src/app/_components/discussions/Discussion.tsx
@@ -4,20 +4,23 @@ import Link from "next/link";
 
 type Props = {
   name: string;
-  img: string;
-  date: string;
+  date: Date;
   answers: number;
 };
 
-export default function Discussion({ name, img, date, answers }: Props) {
+export default function Discussion({ name, answers }: Props) {
   return (
     <Card key={name} className="p-3 flex w-full">
       <div className="flex justify-between items-center gap-2">
         <div className="flex gap-2 justify-center items-center">
           <Avatar className="size-14 border bg-white items-center justify-center">
-            <AvatarImage src={img} alt="Heladera" className="size-10" />
+            <AvatarImage
+              src={"discussion.svg"}
+              alt="Heladera"
+              className="size-10"
+            />
           </Avatar>
-          <Link href="discussion/" className="">
+          <Link href={`/discussion/${name}`} className="">
             <span className="text-sm font-medium text-blue-800 underline">
               {name}
             </span>
@@ -25,7 +28,7 @@ export default function Discussion({ name, img, date, answers }: Props) {
         </div>
         <div className="flex flex-col-reverse items-center lg:flex-row gap-2 text-xs text-foreground">
           <p className="truncate">{answers} respuestas</p>
-          <p>{date}</p>
+          {/* <p>{date}</p> */}
         </div>
       </div>
     </Card>

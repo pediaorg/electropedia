aaddd8f89f8c9720f474df12eaa0df6f76f54255
Santiago Ferreyra <sanferreyra@uade.edu.ar>
2025-07-14 19:04:11 -0300

    fix: discussion date added to discussions

--- src/app/_components/discussions/Discussion.tsx
+++ src/app/_components/discussions/Discussion.tsx
@@ -9,9 +9,9 @@ type Props = {
   answers: number;
 };
 
-export default function Discussion({ id, name, answers }: Props) {
+export default function Discussion({ id, name, date, answers }: Props) {
   return (
-    <Card key={name} className="p-3 flex w-full">
+    <Card key={name} className="p-3 flex w-full bg-input">
       <div className="flex justify-between items-center gap-2">
         <div className="flex gap-2 justify-center items-center">
           <Avatar className="size-14 border bg-white items-center justify-center">
@@ -22,14 +22,14 @@ export default function Discussion({ id, name, answers }: Props) {
             />
           </Avatar>
           <Link href={`/discussion/${id}`} className="">
-            <span className="text-sm font-medium text-blue-800 underline">
+            <span className="text-sm font-medium text-blue-600 underline">
               {name}
             </span>
           </Link>
         </div>
         <div className="flex flex-col-reverse items-center lg:flex-row gap-2 text-xs text-foreground">
           <p className="truncate">{answers} respuestas</p>
-          {/* <p>{date}</p> */}
+          <p>{date.toLocaleDateString()}</p>
         </div>
       </div>
     </Card>

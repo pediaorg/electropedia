cdaaea603982fe47f05ec5960b7a2b53751378a1
Juan I. Casareski <juani.casareski@gmail.com>
2025-07-15 15:35:12 -0300

    feat: close new answer popup on submit

--- src/app/_components/dialogs/NewAnswer.tsx
+++ src/app/_components/dialogs/NewAnswer.tsx
@@ -26,45 +26,49 @@ export default function NewAnswer() {
   };
 
   return (
-    <div className="flex flex-col gap-5">
-      <div className="border-b border-secondary pb-5">
-        <h1 className="text-5xl font-bold text-center w-full">Respuesta</h1>
-      </div>
-      <div className="flex flex-col gap-3">
-        <Card className="flex flex-row items-center bg-input p-2 w-full justify-between">
-          <div className="flex gap-2">
-            <Bold className="size-5" />
-            <Italic className="size-5" />
-            <Link2 className="size-5" />
-            <Image className="size-5" />
-            <div className="border-l-2 border-secondary px-1">
-              <Plus className="size-5" />
+    <form>
+      <div className="flex flex-col gap-5">
+        <div className="border-b border-secondary pb-5">
+          <h1 className="text-5xl font-bold text-center w-full">Respuesta</h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <Card className="flex flex-row items-center bg-input p-2 w-full justify-between">
+            <div className="flex gap-2">
+              <Bold className="size-5" />
+              <Italic className="size-5" />
+              <Link2 className="size-5" />
+              <Image className="size-5" />
+              <div className="border-l-2 border-secondary px-1">
+                <Plus className="size-5" />
+              </div>
             </div>
-          </div>
 
-          <Button
-            className="size-8 rounded-full"
-            variant="ghost"
+            <Button
+              className="size-8 rounded-full"
+              variant="ghost"
+              disabled={answerMutation.isPending}
+              type="submit"
+              onClick={handleNewAnswer}
+            >
+              {answerMutation.isPending ? (
+                <Loader2Icon className="size-5" />
+              ) : (
+                <Send className="size-5" />
+              )}
+            </Button>
+          </Card>
+
+          <Input
+            type="answer"
+            id="answer"
+            autoFocus
+            placeholder="Escriba su respuesta..."
+            onChange={(e) => setAnswer(e.target.value)}
+            className="bg-input border-border"
             disabled={answerMutation.isPending}
-            onClick={handleNewAnswer}
-          >
-            {answerMutation.isPending ? (
-              <Loader2Icon className="size-5" />
-            ) : (
-              <Send className="size-5" />
-            )}
-          </Button>
-        </Card>
-
-        <Input
-          type="answer"
-          id="answer"
-          placeholder="Escriba su respuesta..."
-          onChange={(e) => setAnswer(e.target.value)}
-          className="bg-input border-border"
-          disabled={answerMutation.isPending}
-        />
+          />
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
